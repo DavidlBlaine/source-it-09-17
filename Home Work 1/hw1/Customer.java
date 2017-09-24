7278353bd35dedbfd4d59714a95913dc3824a561
DavidlBlaine <32216742+DavidlBlaine@users.noreply.github.com>
2017-09-24 11:22:38 +0300

    Add files via upload

---
+++ Home Work 1/hw1/Customer.java
@@ -0,0 +1,7 @@
+package hw1;
+
+public class Customer extends Person {
+    public static void main(String[] args) {
+        System.out.println("I`m customer");
+    }
+}
---
+++ Home Work 1/hw1/Person.java
@@ -0,0 +1,7 @@
+package hw1;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("I`m human");
+    }
+}
